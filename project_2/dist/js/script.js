f980bdec2a4dcfc6809d3dbf3813d2fd0ffe5e69
Sergey Kovalchuk <info@skovalchuk.ru>
2020-04-15 14:52:24 +0300

    upd media

--- project_2/dist/js/script.js
+++ project_2/dist/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 575,
+                breakpoint: 992,
                 settings: {
                     dots: false,
                     arrows: false
